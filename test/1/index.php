e776cc617d05a8e762380713d5350c008ae6e4c4
panisa-cake <113135741+panisa-cake@users.noreply.github.com>
2022-09-20 17:42:27 +0430

    Add files via upload

---
+++ test/1/index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Page</title>
+<meta name="generator" content="WYSIWYG Web Builder 17 Trial Version - https://www.wysiwygwebbuilder.com">
+<link href="Untitled1.css" rel="stylesheet">
+<link href="index.css" rel="stylesheet">
+<script src="jquery-3.6.0.min.js"></script>
+<script src="jquery-ui.min.js"></script>
+<script src="wwb17.min.js"></script>
+</head>
+<body onload="ShowObject('Layer4', 1);return false;">
+<div id="container">
+<a href="https://www.wysiwygwebbuilder.com" target="_blank"><img src="images/builtwithwwb17.png" alt="WYSIWYG Web Builder" style="position:absolute;left:441px;top:967px;margin: 0;border-width:0;z-index:250" width="16" height="16"></a>
+<div id="wb_Image2" style="position:absolute;left:0px;top:0px;width:1272px;height:715px;z-index:2;">
+<img src="images/1612225074_67-p-fioletovii-fon-windows-10-104.jpg" loading="lazy" id="Image2" alt="" width="1272" height="716"></div>
+<div id="wb_Image1" style="position:absolute;left:12px;top:0px;width:47px;height:47px;z-index:3;">
+<a href="#" onclick="ShowObject('Layer1', 1);return false;"><img src="images/Bu-bilgisayar-768x768.png" id="Image1" alt="" width="47" height="47"></a></div>
+<div id="wb_Text1" style="position:absolute;left:0px;top:47px;width:250px;height:16px;z-index:4;">
+<span style="color:#F5F5F5;font-family:Tahoma;font-size:13px;">My computer</span></div>
+<div id="Layer1" style="position:absolute;text-align:left;visibility:hidden;left:150px;top:85px;width:744px;height:397px;z-index:5;">
+<div id="Layer2" style="position:absolute;text-align:left;left:0px;top:0px;width:744px;height:27px;z-index:0;">
+</div>
+</div>
+<div id="Layer3" style="position:absolute;text-align:left;left:0px;top:680px;width:1272px;height:35px;z-index:6;">
+</div>
+<div id="Layer4" style="position:absolute;text-align:left;visibility:hidden;left:0px;top:0px;width:1272px;height:715px;z-index:7;" onmouseenter="ShowObjectWithEffect('Layer4', 0, 'fade', 250);return false;">
+</div>
+</div>
+</body>
+</html>
